d358498637edaa272a03b27135ea29830180cdf0
mashitomo0504-arch <mashitomo0504@gmail.com>
2026-04-15 23:59:40 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "My First App",
+  description: "Simple app"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
